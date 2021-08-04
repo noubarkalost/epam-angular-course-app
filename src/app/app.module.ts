248551933a37a1923dd5f46c37f2d06009ce0495
noubar <noubar.kaloust@gmail.com>
2021-08-04 19:45:39 +0400

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import  {ProvidersViewProvidersModule} from "./providers-view-providers/providers-view-providers.module";
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProvidersViewProvidersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/providers-view-providers/providers-view-providers.module.ts
+++ src/app/providers-view-providers/providers-view-providers.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChildComponent } from './child/child.component';
 import { InspectorComponent } from './inspector/inspector.component';
+import { ProvidersViewProvidersComponent } from './providers-view-providers.component';
 
 
 
 @NgModule({
   declarations: [
     ChildComponent,
-    InspectorComponent
+    InspectorComponent,
+    ProvidersViewProvidersComponent
   ],
   imports: [
     CommonModule
