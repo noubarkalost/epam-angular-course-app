462b1e5b852ce3bc811a0a1b2845b8f33694e1f1
noubar <noubar.kaloust@gmail.com>
2021-08-04 19:48:44 +0400

    generated resolution modefier module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import  {ProvidersViewProvidersModule} from "./providers-view-providers/providers-view-providers.module";
+import {ResolutionModifiersModule} from "./resolution-modifiers/resolution-modifiers.module";
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ProvidersViewProvidersModule
+    ProvidersViewProvidersModule,
+    ResolutionModifiersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
